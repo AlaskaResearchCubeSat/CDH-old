95ab681272cb332c8475393f1c7356d2c845b42a
Jesse Frey <jmfrey@alaska.edu>
2013-09-05 13:49:59 -0800

    Added commands from commandLib

--- CDH_errors.h
+++ CDH_errors.h
@@ -1,11 +1,9 @@
 #ifndef __CDH_ERRORS_H
   #define __CDH_ERRORS_H
-  
+  #include <commandLib.h>
   //error sources for BUS test program
-  enum{CDH_ERR_SRC_CMD=ERR_SRC_SUBSYSTEM};
+  enum{CDH_ERR_SRC_CMD=ERR_SRC_SUBSYSTEM+1};
     
-  //command errors
-  enum{CMD_ERR_RESET};
     
   //subsystem errors
   enum{SUB_ERR_SPI_CRC};
